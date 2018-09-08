936f90712d1b9261fd49154de4a35775c6c2f492
Hanbo <wanliuguohanbo@163.com>
2018-09-08 22:19:48 +0800

    6001x_W1

---
+++ Assignment/6001x_W1.py
@@ -0,0 +1,25 @@
+import re
+from itertools import count
+def vowels():
+    a=input("Please type a sentence: ")
+    return sum(map(a.lower().count,"aeiou"))
+
+def overlapping_string_count():
+    s=input("Please type a sentence: ")
+    return len(re.findall('(?=bob)',s))
+
+def find_long_alpha():
+    s="Longestsubstringinalphabeticalorder"
+    a=''
+    b=''
+    c=''
+    for i in s:
+        if a<=i:
+            a=i
+            b+=a
+            if len(b)>len(c):
+                c=b
+        else:
+            a=i
+            b=a
+    return c
