23abdbda491e56f1e721c4a6ba520cd5ede297e9
Hanbo <wanliuguohanbo@163.com>
2018-11-04 18:00:02 +0800

    sorting

--- Assignment/11HW/drawing.py
+++ Assignment/11HW/drawing.py
@@ -5,7 +5,9 @@ import numpy as np
 import time
 import itertools as it
 
-def insertion_sort(list1):
+def insertion(list1):
+    '''list1 -> list object
+    Return sorted list1 with insertion sort'''
     n=len(list1)
     for i in range(n):
         for j in range(i):
@@ -14,7 +16,9 @@ def insertion_sort(list1):
                 break
     return list1
 
-def selection_sort(list1):
+def selection(list1):
+    '''list1 -> list object
+    Return sorted list1 with selection sort'''
     for i in range(len(list1)):
         x=i
         for j in range(1,len(list1)):
@@ -23,34 +27,38 @@ def selection_sort(list1):
         list1[i],list1[x]=list1[x],list1[i]
     return list1
 
-def bubble_sort(list1):
+def bubble(list1):
+    '''list1 -> list object
+    Return sorted list1 with bubble sort'''
     for i in range(len(list1)):
         for j in range(i, len(list1)):
             if list1[i] > list1[j]:
                 list1[i], list1[j] = list1[j], list1[i]
     return list1
 
-def alg(a,b):
-    result=[]
-    while len(a) and len(b):
-        if a[0]<=b[0]:
-            result.append(a.pop(0))
-        elif b[0]<=a[0]:
-            result.append(b.pop(0))
-    if len(a)!=0:
-        result+=a
-    elif len(b)!=0:
-        result+=b
-    return result
-        
-    
-def merge_sort(list1):
-    if len(list1)==1:
-        return list1
-    mid=len(list1)//2
-    a=merge_sort(list1[:mid])
-    b=merge_sort(list1[mid:])
-    return alg(a,b)
+def merge(list1):
+    '''list1 -> list object
+    Return sorted list1 with merge sort'''
+    def alg(a,b):
+        result=[]
+        while len(a) and len(b):
+            if a[0]<=b[0]:
+                result.append(a.pop(0))
+            elif b[0]<=a[0]:
+                result.append(b.pop(0))
+        if len(a)!=0:
+            result+=a
+        elif len(b)!=0:
+            result+=b
+        return result    
+    def merge_sort(list1):
+        if len(list1)==1:
+            return list1
+        mid=len(list1)//2
+        a=merge_sort(list1[:mid])
+        b=merge_sort(list1[mid:])
+        return alg(a,b)
+    merge_sort(list1)
 
 def timing(f,l):
     a=list(np.random.random_sample(l))
@@ -68,10 +76,10 @@ def drawing():
     x=[1000,2000,3000,4000,5000]
     data=[[],[],[],[]]
     for i in ts:
-        data[0].append(timing(insertion_sort,i))
-        data[1].append(timing(selection_sort,i))
-        data[2].append(timing(bubble_sort,i))
-        data[3].append(timing(merge_sort,i))
+        data[0].append(timing(insertion,i))
+        data[1].append(timing(selection,i))
+        data[2].append(timing(bubble,i))
+        data[3].append(timing(merge,i))
     l1,=plt.plot(x,data[0],color="red",label="insection")
     l2,=plt.plot(x,data[1],color="green",label="selection")
     l3,=plt.plot(x,data[2],color="blue",label="bubble")
---
+++ Assignment/sorting.py
@@ -0,0 +1,85 @@
+import numpy
+import time
+import unittest
+
+class sorting(object):
+    def insertion(list(list1)):
+        '''list1 -> list object
+        Return sorted list1 with insertion sort'''
+        n=len(list1)
+        insert,pop=list1.insert,list1.pop
+        for i in range(n):
+            for j in range(i):
+                if list1[i] < list1[j]:
+                    insert(j, pop(i))
+                    break
+
+    def selection(list1):
+        '''list1 -> list object
+        Return sorted list1 with selection sort'''
+        n=len(list1)
+        for i in range(n):
+            x=i
+            for j in range(1,n):
+                if list1[j]<list1[i]:
+                    x=j
+            list1[i],list1[x]=list1[x],list1[i]
+        return list1
+
+    def bubble(list(list1)):
+        '''list1 -> list object
+        Return sorted list1 with bubble sort'''
+        n=len(list1)
+        for i in range(n):
+            for j in range(i, n):
+                if list1[i]>list1[j]:
+                    list1[i],list1[j]=list1[j],list1[i]
+        return list1
+
+    def merge(list(list1)):
+        '''list1 -> list object
+        Return sorted list1 with merge sort'''
+        def alg(a,b):
+            result=[]
+            append=result.append
+            popa,popb=a.pop,b.pop
+            while len(a) and len(b):
+                if a[0]<=b[0]:
+                    append(popa(0))
+                elif b[0]<=a[0]:
+                    append(popb(0))
+            if len(a)!=0:
+                result+=a
+            elif len(b)!=0:
+                result+=b
+            return result    
+        def merge_sort(list1):
+            n=len(list1)
+            if n==1:
+                return list1
+            mid=n//2
+            a=merge_sort(list1[:mid])
+            b=merge_sort(list1[mid:])
+            return alg(a,b)
+        merge_sort(list1)
+
+    def quick(list(list1)):
+        n=len(list1)
+        def sorting(left,right):
+            if left>right:
+                return
+            l,r=left,right
+            pivot=list1[l]
+            while l<r:
+                while l<r and list1[r]>pivot:
+                    r-=1
+                while l<r and list1[l]<=pivot:
+                    l+=1
+                list1[l],list1[r]=list1[r],list1[l]
+            list1[l],list1[left]=pivot,list1[l]
+            sorting(left,l-1)
+            sorting(r+1,right)
+        sorting(0,n-1)
+        return list1
+
+        
